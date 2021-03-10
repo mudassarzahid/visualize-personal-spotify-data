59cdbb90ef97bdfe2cf4b0510a73080ba4a593fc
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-03-10 21:50:51 +0100

    Add public directory

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly as py
 import plotly.graph_objs as go
 
 # replace 'example_data.json' with 'StreamingHistory.json', merge all StreamingHistory files if you received multiple
-with open('example_data.json', 'r') as json_data:
+with open('public/example_data.json', 'r') as json_data:
   data = json.load(json_data)
 
 
